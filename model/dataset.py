dde5dfce88ebb28ee73257bdf79c0c9a312708d9
endingback <18120833@bjtu.edu.cn>
2020-05-27 00:24:15 +0800

    dataset final

--- model/dataset.py
+++ model/dataset.py
@@ -68,3 +68,187 @@ class Dataset(object):
                 #赋值操作
                 while num < self.batch_size:
                     index = self.batch_count
+		    annotation = self.annotation[index]
+                    #生成图像、标注, data augment
+                    image, bboxes = self.parse_annotation(annotation)
+                    label_sbbox, label_mbbox, label_lbbox, sbboxes, mbboxes, lbboxes = self.preprocess_true_boxes(bboxes)
+                    #add batch axis
+                    batch_image[num, :, :, :] = image
+                    batch_label_sbbox[num, :, :, :, :] = label_sbbox
+                    batch_label_lbbox[num, :, :, :, :] = label_lbbox
+                    batch_label_mbbox[num, :, :, :, :] = label_mbbox
+
+                    batch_sbboxes[num, :, :] = sbboxes
+                    batch_mbboxes[num, :, :] = mbboxes
+                    batch_lbboxes[num, :, :] = lbboxes
+                    num += 1
+                self.batch_count += 1
+                return batch_image, batch_label_mbbox, batch_label_lbbox, batch_label_sbbox, batch_sbboxes, batch_mbboxes, batch_lbboxes
+
+            else:
+                self.batch_count = 0
+                np.random.shuffle(self.annotation)
+                raise StopIteration
+
+    def preprocess_true_boxes(self, bboxes):
+        '''
+        make bboxes to different scale size(3)
+        :param bboxes:
+        :return:
+        '''
+        #label(output_size, output_size, anchor_per_scale, 5 + classes)
+        label = [np.zeros((self.train_output_sizes[i], self.train_output_sizes[i], self.anchor_per_scale,
+                           5 + self.num_classes)) for i in range(3)]
+        bboxes_xywh = [np.zeros((self.max_bbox_per_scale, 4)) for _ in range(3)]
+        #compute different scale of bbox count for predict annotation
+        bbox_count = np.zeros((3,))
+        for bbox in bboxes:
+            bbox_coor = bbox[:4]
+            bbox_class_ind = bbox[4]
+            #each anchor include classes
+            onehot = np.zeros(self.num_classes, dtype=np.float)
+            #muliticlass to one_hot
+            onehot[bbox_class_ind] = 1.0
+            uniform_distribution = np.full(self.num_classes, 1 / self.num_classes)
+            deta = 0.01
+            #not absolute put one_hot
+            smooth_onehot = onehot * (1 - deta) + deta * uniform_distribution
+            #center point width and length
+            bbox_xywh = np.concatenate([(bbox_coor[2:] + bbox_coor[:2]) * 0.5, bbox_coor[2:] - bbox_coor[:2]], axis = -1)
+            #annotaions to 3 scaled size
+            bbox_xywh_scaled = 1.0 * bbox_xywh[np.newaxis, :] / self.strides[:, np.newaxis]
+
+            iou = []
+            exist_positive = False
+            #three different scale iou with annotation > 0.3
+            for i in range(3):
+                anchor_xywh = np.zeros((self.anchor_per_scale, 4))
+                anchor_xywh[:, 0:2] = np.floor(bbox_xywh_scaled[i, 0:2]).astype(np.int32) + 0.5
+                anchor_xywh[:, 2:4] = self.anchors[i]
+                #compute iou
+                iou_scale = self.bbox_iou(bbox_xywh_scaled[i][np.newaxis, :], anchor_xywh)
+                iou.append(iou_scale)
+                iou_mask = iou_scale > 0.3
+
+                if np.any(iou_mask):
+                    xind, yind = np.floor(bbox_xywh_scaled[i, 0:2]).astype(np.int32)
+                    label[i][yind, xind, iou_mask, :] = 0
+                    label[i][yind, xind, iou_mask, 0:4] = bbox_xywh
+                    label[i][yind, xind, iou_mask, 4:5] = 1.0
+                    label[i][yind, xind, iou_mask, 5:] = smooth_onehot
+
+                    bbox_ind = int(bbox_count[i] % self.max_bbox_per_scale)
+                    bboxes_xywh[i][bbox_ind, :4] = bbox_xywh
+                    bbox_count[i] += 1
+
+                    exist_positive = True
+            if not exist_positive:
+                best_anchor_ind = np.argmax(np.array(iou).reshape(-1), axis=-1)
+                best_detect = int(best_anchor_ind / self.anchor_per_scale)
+                best_anchor = int(best_anchor_ind % self.anchor_per_scale)
+                xind, yind = np.floor(bbox_xywh_scaled[best_detect, 0:2]).astype(np.int32)
+
+                label[best_detect][yind, xind, best_anchor, :] = 0
+                label[best_detect][yind, xind, best_anchor, 0:4] = bbox_xywh
+                label[best_detect][yind, xind, best_anchor, 4:5] = 1.0
+                label[best_detect][yind, xind, best_anchor, 5:] = smooth_onehot
+
+                bbox_ind = int(bbox_count[best_detect] % self.max_bbox_per_scale)
+                bboxes_xywh[best_detect][bbox_ind, :4] = bbox_xywh
+                bbox_count[best_detect] += 1
+        label_sbbox, label_mbbox, label_lbbox = label
+        sbboxes, mbboxes, lbboxes = bboxes_xywh
+
+        return label_sbbox, label_mbbox, label_lbbox, sbboxes, mbboxes, lbboxes
+
+    def bbox_iou(self, boxes1, boxes2):
+        boxes1 = np.array(boxes1)
+        boxes2 = np.array(boxes2)
+
+        boxes1_area = boxes1[..., 2] * boxes1[..., 3]
+        boxes2_area = boxes2[..., 2] * boxes2[..., 3]
+        #xyhw to xyxy
+        boxes1 = np.concatenate([boxes1[..., :2] - boxes1[..., 2:] * 0.5,
+                                 boxes1[..., :2] + boxes1[..., 2:] * 0.5], axis=-1)
+        boxes2 = np.concatenate([boxes2[..., :2] - boxes2[..., 2:] * 0.5,
+                                 boxes2[..., :2] + boxes2[..., 2:] * 0.5], axis=-1)
+        left_up = np.minimum(boxes1[..., :2], boxes2[..., :2])
+        right_down = np.maximum(boxes1[..., 2:], boxes2[..., 2:])
+
+        inter_section = np.maximum(right_down - left_up, 0.0)
+        inter_area = inter_section[..., 0] * inter_section[..., 1]
+        union_area = boxes1_area + boxes2_area - inter_area
+
+        return inter_area / union_area
+
+    def parse_annotation(self, annotation):
+        line = annotation.split()
+        image_path = line[0]
+        if not os.path.exists(image_path):
+            raise KeyError("%s dose not exists ..." %image_path)
+        image = np.array(cv2.imread(image_path))
+        bboxes = np.array([list(map(lambda x:int(float(x)), box.split(','))) for box in line[1:]])
+        # data augment
+        if self.data_aug:
+            image, bboxes = self.random_horizontal_flip(np.copy(image), np.copy(bboxes)) #np.copy 深拷贝
+            image, bboxes = self.random_crop(np.copy(image), np.copy(bboxes))
+            image, bboxes = self.random_translate(np.copy(image), np.copy(bboxes))
+
+        image, bboxes = utils.image_preprocess(np.copy(image), [self.train_input_size, self.train_input_size], np.copy(bboxes))
+        return image, bboxes
+
+    def random_horizontal_flip(self, image, bboxes):
+        if random.random() < 0.5:
+            _, w, _ = image.shape
+            #左右翻转
+            image = image[:, ::-1, :]
+            bboxes[:, [0, 2]] = w - bboxes[:, [2, 0]]
+
+        return image, bboxes
+
+    def random_crop(self, image, bboxes):
+        if random.random() < 0.5:
+            h, w, _ = image.shape
+            #找到一个矩形框，这个框比所有的框的左上角的点小，比右下角的点大
+            max_bbox = np.concatenate([np.min(bboxes[:, 0 : 2], axis=0), np.max(bboxes[:, 2:4], axis=0)], axis=-1)
+
+            max_l_trans = max_bbox[0]
+            max_u_trans = max_bbox[1]
+            max_r_trans = w - max_bbox[2]
+            max_d_trans = h - max_bbox[3]
+
+            crop_xmin = max(0, int(max_bbox[0] - random.uniform(0, max_l_trans)))
+            crop_ymin = max(0, int(max_bbox[1] - random.uniform(0, max_u_trans)))
+            crop_xmax = max(0, int(max_bbox[2] + random.uniform(0, max_r_trans)))
+            crop_ymax = max(0, int(max_bbox[3] + random.uniform(0, max_d_trans)))
+
+            image = image[crop_xmin : crop_xmax, crop_ymin : crop_ymax]
+
+            bboxes[:, [0, 2]] = bboxes[:, [0, 2]] - crop_xmin
+            bboxes[:, [1, 3]] = bboxes[:, [1, 3]] - crop_ymin
+        return image, bboxes
+
+    def random_translate(self, image, bboxes):
+
+        if random.random() < 0.5:
+            h, w, _ = image.shape
+            max_bbox = np.concatenate([np.min(bboxes[:, 0:2], axis=0), np.max(bboxes[:, 2:4], axis=0)], axis=-1)
+            max_l_trans = max_bbox[0]
+            max_u_trans = max_bbox[1]
+            max_r_trans = w - max_bbox[2]
+            max_d_trans = h - max_bbox[3]
+
+            tx = random.uniform(-(max_l_trans - 1), (max_r_trans - 1))
+            ty = random.uniform(-(max_u_trans - 1), (max_d_trans - 1))
+
+            M = np.array([[1, 0, tx], [0, 1, ty]])
+            #仿射变换
+            image = cv2.warpAffine(image, M, (w, h))
+
+            bboxes[:, [0, 2]] = bboxes[:, [0, 2]] + tx
+            bboxes[:, [1, 3]] = bboxes[:, [1, 3]] + ty
+
+        return image, bboxes
+
+    def __len__(self):
+        return self.num_batch
